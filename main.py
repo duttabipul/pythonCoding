49a0b58950c2370d2f12ba78624f0474ea6a181d
Bipul Dutta <106006174+duttabipul@users.noreply.github.com>
2022-08-02 17:57:05 +0600

    added another line

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def verify(index):
 numbers = [1,2,3,4,5,6,7,8,9]
 result = linear_search(numbers, 9)
 verify(result)
-"""
+
 #02.08.2022
 
 # Binary Search:
@@ -83,3 +83,26 @@ def verify(index):
 numbers = [1,2,3,4,5,6,7,8,9]
 result = binary_search(numbers, 7)
 verify(result)
+"""
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+    while first <= last:
+        midpoint = (first + last)//2
+
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint + 1
+
+def verify(index):
+    if index is not None:
+        print("Target is found in list: ", index)
+    else:
+        print("Target not found in list")
+numbers = [1,2,3,4,5,6,7,8,9]
+result = binary_search(numbers, 7)
+verify(result)
+#3
