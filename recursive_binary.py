293713b02fa04ea16a11264d0d91f8252f47d4ce
Bipul Dutta <106006174+duttabipul@users.noreply.github.com>
2022-08-02 18:59:19 +0600

    updated all files

--- recursive_binary.py
+++ recursive_binary.py
@@ -1,3 +1,4 @@
+"""
 #02.08.2022
 
 def recursive_binary_search(list, target):
@@ -25,3 +26,31 @@ verify(result)
 
 result = recursive_binary_search(numbers, 5)
 verify(result)
+"""
+#02
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = len(list)//2
+
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint+1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint], target)
+def verify(index):
+    if index is not None:
+        print("Target is found in list: ", index)
+    else:
+        print("Target is not found in list")
+
+numbers = [1,2,3,4,5,6,7,8,9]
+result = recursive_binary_search(numbers, 8)
+verify(result)
+
+result = recursive_binary_search(numbers, 15)
+verify(result)
+#4
