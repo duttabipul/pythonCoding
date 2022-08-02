d27c0f267e30655becdde7561043825614257e90
Bipul Dutta <106006174+duttabipul@users.noreply.github.com>
2022-08-02 22:47:17 +0600

    added a new file name arrays
    added a new file name arrays from home.

---
+++ arrays.py
@@ -0,0 +1,20 @@
+"""
+#02.08.2022
+
+new_list = [1, 2 , 3]
+result = new_list[0]
+print(result)
+
+if 1 in new_list: print(True)
+
+for n in new_list:
+    if n == 1:
+        print(True)
+
+        break
+"""
+numbers = []
+#list1 = len(numbers)
+numbers.append(2)
+print(list1)
+print(numbers)
