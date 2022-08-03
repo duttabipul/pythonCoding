9b13189acf3a88671a294e683b133259804dddb0
Bipul Dutta <106006174+duttabipul@users.noreply.github.com>
2022-08-03 19:12:46 +0600

    added app file
    added app file: 03.08.2022

---
+++ app.py
@@ -0,0 +1,34 @@
+#03.08.2022
+"""
+print("Hello World")
+#01:
+name = input("input name: ")
+age = input("enter age: ")
+print(name, "is a new patient and", age,  " years old")
+#02:
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year)
+print(age)
+
+#03:
+num1 = 10.5
+num2 = 20
+sum = num1 + num2
+print(sum)
+
+#04:
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
+print(course.find('a'))
+print(course.replace('for', 'and'))
+"""
+#05:
+x = 10 + 3 * 2
+print(x)
+y = 3 == 2
+print(y)
+
+price = 20
+print(price < 15)
+print(price>10)
