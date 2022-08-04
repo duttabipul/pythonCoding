1d7759b65ac7a627eebf1b681779a5a04c30677b
Bipul Dutta <106006174+duttabipul@users.noreply.github.com>
2022-08-04 12:09:27 +0600

    updated app file

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ print(course.upper())
 print(course.lower())
 print(course.find('a'))
 print(course.replace('for', 'and'))
-"""
+
 #05:
 x = 10 + 3 * 2
 print(x)
@@ -32,3 +32,74 @@ print(y)
 price = 20
 print(price < 15)
 print(price>10)
+temperature = 20
+if temperature > 30:
+    print("It's a hot day")
+    print("Drink plenty of water")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("Its a normal day")
+print("Done")
+# 06:
+weight = input("Weight: ")
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = float(weight) / 0.45
+    print("Weight in Pounds: ", converted)
+else:
+    converted = float(weight) * 0.45
+    print("Weight in Kg: ", converted)
+weight = input("Weight: ")
+unit = input("(k)g or (l)bs: ")
+if unit.upper() == "K":
+    converted = float(weight) / 0.45
+    print("Wight in Pound: ", converted)
+else:
+    converted = float(weight) * 0.45
+    print("Weight in Kg: ", converted)
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+j = 1
+while j <= 10:
+    print(j * "*")
+    j += 1
+
+# 04.08.2022
+names = ["John", "Bob", "Mary", "Rock"]
+names[1] = "Rob"
+print(names)
+
+list = [1,2,3,4,5]
+list.append(6)
+list.remove(3)
+print(list)
+print(len(list))
+
+numbers = [1,2,3,4,5,6]
+
+for item in numbers:
+    print(item)
+
+numbers = [1,2,3,4,5,6]
+
+i = 0
+while i < len(numbers):
+    print(i)
+    i = i + 1
+
+numbers = range(5, 20)
+
+for number in numbers:
+    print(number)
+
+numbers = range(2, 15)
+
+for num in numbers:
+    print(num)
+"""
+numbers = (1,2,3,4,5)
+numbers.index(2)
+print(numbers)
